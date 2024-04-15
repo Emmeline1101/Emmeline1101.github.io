7fd0d4d0342c03e7a52c12cb6e79754b380f45c2
Emmeline1101 <emmelinexu23@gmail.com>
2024-04-15 00:58:54 -0700

    test

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //script.js
-document.addEventListener('')
+document.addEventListener('DOMContentLoaded', function(){
+    const courseBtns = document.querySelectorAll('.course-btn');
+    courseBtns.forEach((btn) => {
+        btn.addEventListener('click', () =>{
+            const courseList = btn.previousElementSibling;
+            if (courseList.style.display === 'none'){
+                courseList.style.display = 'block';
+                btn.textContent = 'View Less';
+            } else {
+                courseList.style.display = 'none';
+                btn.textContent = 'View More';
+            }
+        });
+    });
+});
